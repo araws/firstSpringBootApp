44e6037cc45d5223ac90b39c052995a83e3c590c
araws <rawski.andrzej@gmail.com>
2019-12-02 21:58:32 +0100

    Add @GetMapping

--- src/main/java/pl/antygravity/firstspringbootapp/CarApi.java
+++ src/main/java/pl/antygravity/firstspringbootapp/CarApi.java
@@ -1,12 +1,23 @@
 package pl.antygravity.firstspringbootapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CarApi {
 
-    @Autowired
-    private CarManager carManager;
+    private final CarManager carManager;
 
+    @Autowired
+    public CarApi(CarManager carManager) {
+        this.carManager = carManager;
+    }
+
+    @GetMapping("/getCars")
+    public List<Car> getCars() {
+        return carManager.getCarList();
+    }
 }
