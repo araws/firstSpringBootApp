2989233208c55c7937a8f7dc64c07bc8ed70505e
araws <rawski.andrzej@gmail.com>
2019-12-02 13:53:51 +0100

    Add adnotations

--- src/main/java/pl/antygravity/firstspringbootapp/CarManager.java
+++ src/main/java/pl/antygravity/firstspringbootapp/CarManager.java
@@ -1,8 +1,11 @@
 package pl.antygravity.firstspringbootapp;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarManager {
 
     private List<Car> carList;
@@ -12,5 +15,13 @@ public class CarManager {
         carList.add(new Car("Citroen", "C4"));
         carList.add(new Car("Citroen", "Berlingo"));
         carList.add(new Car("Audi", "A8"));
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(List<Car> carList) {
+        this.carList = carList;
     }
 }
